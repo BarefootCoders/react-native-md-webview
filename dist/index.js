540042f41ed470da4fc32b5729980f0b21aeeafb
Kevin Jasker <kevinjasker@gmail.com>
2016-08-16 18:33:04 -0700

    update react imports to be react-native@0.30.0 compatible

--- dist/index.js
+++ dist/index.js
@@ -6,21 +6,17 @@ Object.defineProperty(exports, "__esModule", {
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var _reactNative = require('react-native');
+var _react = require('react');
 
-var _reactNative2 = _interopRequireDefault(_reactNative);
+var _react2 = _interopRequireDefault(_react);
+
+var _reactNative = require('react-native');
 
 var _defaultHtml = require('./default-html');
 
 var _defaultHtml2 = _interopRequireDefault(_defaultHtml);
 
-var _lodash = require('lodash.omit');
-
-var _lodash2 = _interopRequireDefault(_lodash);
-
-var _lodash3 = require('lodash.pick');
-
-var _lodash4 = _interopRequireDefault(_lodash3);
+var _lodash = require('lodash');
 
 var _showdown = require('showdown');
 
@@ -30,11 +26,11 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var REMOVED_WEBVIEW_PROPS = ['url', 'html', 'source'];
 
-exports.default = _reactNative2.default.createClass({
+exports.default = _react2.default.createClass({
   displayName: 'lib',
 
   propTypes: Object.assign({}, _reactNative.WebView.PropTypes, {
-    children: _reactNative2.default.PropTypes.string.isRequired
+    children: _react2.default.PropTypes.string.isRequired
   }),
 
   getDefaultProps: function getDefaultProps() {
@@ -44,8 +40,8 @@ exports.default = _reactNative2.default.createClass({
     };
   },
   filterProps: function filterProps() {
-    var thisProps = (0, _lodash2.default)(this.props, Object.keys(_reactNative.WebView.propTypes));
-    var webViewProps = (0, _lodash2.default)((0, _lodash4.default)(this.props, Object.keys(_reactNative.WebView.propTypes)), REMOVED_WEBVIEW_PROPS);
+    var thisProps = (0, _lodash.omit)(this.props, Object.keys(_reactNative.WebView.propTypes));
+    var webViewProps = (0, _lodash.omit)((0, _lodash.pick)(this.props, Object.keys(_reactNative.WebView.propTypes)), REMOVED_WEBVIEW_PROPS);
 
     return { thisProps: thisProps, webViewProps: webViewProps };
   },
@@ -67,7 +63,7 @@ exports.default = _reactNative2.default.createClass({
     var markdownHtml = this.markdownHtml();
     var html = _defaultHtml2.default.replace('$body', markdownHtml).replace('$css', this.props.css);
 
-    return _reactNative2.default.createElement(_reactNative.WebView, _extends({
+    return _react2.default.createElement(_reactNative.WebView, _extends({
       source: { html: html, baseUrl: 'about:blank' }
     }, webViewProps));
   }
